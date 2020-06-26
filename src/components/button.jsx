980e45d8109945ec00fae32a2cc5e632d75674c5
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-26 19:03:11 +0700

    Styled Components

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './button.css';
+import styled from 'styled-components';
 
-const Button = () => (
-  <button type="button" className="button">
-    Click me!
-  </button>
-);
+const Button = styled.button`
+  background-color: #ff0000;
+  width: 320px;
+  padding: 20px;
+  border-radius: 5px;
+  border: none;
+  outline: none;
+  &:hover {
+    color: #fff;
+  }
+  &:active {
+    position: relative;
+    top: 2px;
+  }
+  @media (max-width: 480px) {
+    width: 160px;
+  }
+`;
 
 export default Button;
