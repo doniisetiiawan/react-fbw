3ec357363c03d6af619d87909b1fa730812d7822
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-26 14:10:43 +0700

    Inline styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './App.css';
-import Circle from './components/circle';
+import FontSize from './components/font-size';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Circle x={20} y={20} radius={20} fill="blue" />
+        <FontSize />
       </header>
     </div>
   );
---
+++ src/components/font-size.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class FontSize extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 16,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      value: Number(target.value),
+    });
+  };
+
+  render() {
+    return (
+      <input
+        type="number"
+        value={this.state.value}
+        onChange={this.handleChange}
+        style={{ fontSize: this.state.value }}
+      />
+    );
+  }
+}
+
+export default FontSize;
